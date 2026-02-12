36d636c0912352c71ec82a01fdd38021422cfd7a
jprodrigues644 <94868541+jprodrigues644@users.noreply.github.com>
2026-02-12 07:13:38 +0100

    Entities

---
+++ mddback/src/test/java/com/orion/mdd/MddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.mdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
