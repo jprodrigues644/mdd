376ffae0d3b03472aab39b9eb380f78c66c893f7
jprodrigues644 <94868541+jprodrigues644@users.noreply.github.com>
2026-03-10 23:44:41 +0100

    langing page

--- mddfront/mddfront/src/app/app.routes.ts
+++ mddfront/mddfront/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+import { Login } from './pages/login/login';import { Register } from './pages/register/register';
+import { Feed } from './pages/feed/feed';
 
-export const routes: Routes = [];
+
+export const routes:  Routes = [
+
+{ path: '', component: Landing },
+
+{ path: 'login', component: Login },
+
+{ path: 'register', component: Register },
+
+{ path: 'feed', component: Feed }
+
+];
