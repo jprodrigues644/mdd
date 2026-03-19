cb51f1ee47df9e35995c19c32b0a6e361016f725
jprodrigues644 <94868541+jprodrigues644@users.noreply.github.com>
2026-03-19 15:49:45 +0100

    navbar

--- mddfront/mddfront/src/app/shared/components/navbar/navbar.ts
+++ mddfront/mddfront/src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { Auth } from '../../../core/services/auth';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  private auth = inject(Auth);
+  private router = inject(Router);
+
+  logout(): void {
+    this.auth.logout();
+  }
+
+  goToProfile(): void {
+    this.router.navigate(['/profile']);
+  }
+}
