639e6e5acbd9dc61f4b21d26aeacab0deb5c2156
jprodrigues644 <94868541+jprodrigues644@users.noreply.github.com>
2026-03-19 15:49:14 +0100

    update post

--- mddfront/mddfront/src/app/core/services/post.ts
+++ mddfront/mddfront/src/app/core/services/post.ts
@@ -1,6 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PostListResponse } from '../../shared/models/post.model';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class Post {}
+@Injectable({ providedIn: 'root' })
+export class PostService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/posts';
+
+  getFeed(): Observable<PostListResponse[]> {
+    return this.http.get<PostListResponse[]>(`${this.apiUrl}/feed`);
+  }
+}
